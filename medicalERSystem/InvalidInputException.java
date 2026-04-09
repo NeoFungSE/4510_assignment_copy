1adf85702d5c49f1e88db12e2f866ded9c37e1a6
NeoFungSE <250391300@stu.vtc.edu.hk>
2026-04-10 03:15:34 +0800

    Converted to pesudo code version

--- medicalERSystem/InvalidInputException.java
+++
@@ -1,9 +0,0 @@
-package medicalERSystem;
-
-import java.util.InputMismatchException;
-
-public class InvalidInputException extends InputMismatchException {
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
